2463bbed0b4ac5e6cf89c1656a1a150929bd8c2e
suchitharavi <sravi@ip-192-168-47-70.us-gov-west-1.compute.internal>
2018-01-11 09:21:08 -0500

    Added snakeyaml dependency in pom, Created new service class YamlReder.

--- src/main/java/gov/va/ascent/test/framework/util/AppConstants.java
+++ src/main/java/gov/va/ascent/test/framework/util/AppConstants.java
@@ -3,7 +3,7 @@ package gov.va.ascent.test.framework.util;
 public class AppConstants {
 
 	public static final String VAULT_TOKEN_PARAM_NAME = "X-Vault-Token";
-	
+	public static final String VAULT_TOKEN_PARAM_NAMECONFIG = "X-Config-Token";
 	private AppConstants() {
 		
 	}
