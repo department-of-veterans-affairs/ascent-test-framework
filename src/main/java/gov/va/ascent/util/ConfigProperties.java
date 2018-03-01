b31f9701e1a883e270d4d9ceef625f824a76b2dd
armin <aburkholder@meetveracity.com>
2018-03-01 11:15:10 -0500

    RDS-61 directory names, file names, file content: change "vetsapi" or "vets-api" to "vetservices"

---
+++ src/main/java/gov/va/ascent/util/ConfigProperties.java
@@ -0,0 +1,12 @@
+package gov.va.ascent.util;
+
+/**
+ * Singleton to hold the property bag for this project. Call refresh() to repopulate the properties.
+ * @author aburkholder
+ *
+ */
+public class ConfigProperties {
+
+//	private static Properties
+	
+}
